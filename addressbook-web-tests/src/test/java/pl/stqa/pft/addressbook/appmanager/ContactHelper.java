0f3db81fc2aab301475a7a7f9cdd8240b42f747f
mkrupinska <zieloneszkielko@gmail.com>
2017-12-14 00:04:05 +0100

    ContactCreationTest + Helper
    Home work no5

---
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,31 @@
+package pl.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import pl.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase {
+
+  public ContactHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void fillContactForm(ContactData contactData) {
+    type(By.name("firstname"), contactData.getFirstname());
+    type(By.name("lastname"), contactData.getLastname());
+    type(By.name("address"), contactData.getAddress());
+    type(By.name("home"), contactData.getHomephone());
+    type(By.name("email"), contactData.getEmail());
+  }
+
+  public void initAddNewContact(){
+    click(By.linkText("add new"));
+  }
+
+  public void confirmCreation (){
+    click((By.xpath("//div[@id='content']/form/input[21]")));
+  }
+
+
+
+}
