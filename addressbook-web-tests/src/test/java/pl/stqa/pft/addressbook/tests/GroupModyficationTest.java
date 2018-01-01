901e5fdf57b58164be16d25e077b7289a4f686d9
mkrupinska <zieloneszkielko@gmail.com>
2018-01-01 22:19:13 +0100

    kształtowanie kolekcji

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModyficationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModyficationTest.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModyficationTest extends TestBase {
 
 @Test
@@ -13,14 +15,14 @@ public class GroupModyficationTest extends TestBase {
       if (! app.getGroupHelper().isThereAGroup()) {
        app.getGroupHelper().createGroup(new GroupData("test3", null, null));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().SelectGroup(before-1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().SelectGroup(before.size()-1);
     app.getGroupHelper().initGroupModyfication();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModyfication();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-  Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+  Assert.assertEquals(after.size(), before.size());
 
 
 
