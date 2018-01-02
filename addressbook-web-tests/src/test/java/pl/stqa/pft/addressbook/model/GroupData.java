f6f6444d43ec096891a755767fcf0fcc417c86a2
mkrupinska <zieloneszkielko@gmail.com>
2018-01-02 16:31:58 +0100

    Sortowanie list

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
@@ -14,8 +14,10 @@ public class GroupData {
     this.header = header;
     this.footer = footer;
   }
+
+
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -42,21 +44,6 @@ public class GroupData {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-
-    return Objects.hash(id, name);
-  }
-
-  @Override
   public String toString() {
     return "GroupData{" +
             "id='" + id + '\'' +
@@ -64,5 +51,18 @@ public class GroupData {
             '}';
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(name);
+  }
 
 }
