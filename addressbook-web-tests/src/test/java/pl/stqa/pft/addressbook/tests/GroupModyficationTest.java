0ad5d73460b6269514d095f5c7e5fe1f619da851
mkrupinska <zieloneszkielko@gmail.com>
2018-01-02 14:16:09 +0100

    zbiory, lekcja 7

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModyficationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModyficationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModyficationTest extends TestBase {
@@ -18,12 +19,17 @@ public class GroupModyficationTest extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().SelectGroup(before.size()-1);
     app.getGroupHelper().initGroupModyfication();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData(before.get(before.size()-1).getId(),"test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModyfication();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
   Assert.assertEquals(after.size(), before.size());
 
+
+  before.remove(before.size() -1);
+  before.add(group);
+  Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
 
 
