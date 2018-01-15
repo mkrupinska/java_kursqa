a257fc9e0eb64a4a3de62fb12cdd4aba66f30875
mkrupinska <zieloneszkielko@gmail.com>
2018-01-15 16:51:04 +0100

    6.3wiersz polecen

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package pl.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import pl.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -11,13 +14,28 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
+  @Parameter (names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
   public static void main (String [] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    }catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
 
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-
+  private void run() throws IOException {
     List<GroupData> groups = generatorGroups(count);
-    save (groups, file);
+    save (groups, new File(file));
+
   }
 
   private static void save(List<GroupData> groups, File file) throws IOException {
